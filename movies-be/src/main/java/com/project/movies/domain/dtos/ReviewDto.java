28412e91e63d84f4ebd9142b2856d4c3c12da2cd
Harvlin Maximillian <harvlinmaximillian@gmail.com>
2024-12-13 01:56:53 +0700

    Update ReviewDto.java

--- movies-be/src/main/java/com/project/movies/domain/dtos/ReviewDto.java
+++ movies-be/src/main/java/com/project/movies/domain/dtos/ReviewDto.java
@@ -18,37 +18,4 @@ public class ReviewDto {
 
     @JsonIgnore
     MovieDto movieDto;
-
-    public ReviewDto() {
-    }
-
-    public ReviewDto(UUID reviewId, String body, MovieDto movieDto) {
-        this.reviewId = reviewId;
-        this.body = body;
-        this.movieDto = movieDto;
-    }
-
-    public UUID getReviewId() {
-        return reviewId;
-    }
-
-    public void setReviewId(UUID reviewId) {
-        this.reviewId = reviewId;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public MovieDto getMovieDto() {
-        return movieDto;
-    }
-
-    public void setMovieDto(MovieDto movieDto) {
-        this.movieDto = movieDto;
-    }
 }
