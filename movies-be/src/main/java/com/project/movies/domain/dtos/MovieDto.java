2e37f1a394b73fbefae563fd0876cacc273f6cbb
Harvlin Maximillian <harvlinmaximillian@gmail.com>
2024-12-13 01:55:01 +0700

    Update MovieDto.java

--- movies-be/src/main/java/com/project/movies/domain/dtos/MovieDto.java
+++ movies-be/src/main/java/com/project/movies/domain/dtos/MovieDto.java
@@ -1,10 +1,16 @@
 package com.project.movies.domain.dtos;
 
 import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
 import java.util.List;
 import java.util.UUID;
 
 @Data
+@AllArgsContructor
+@NoArgsConstructor
+@Builder
 public class MovieDto {
     UUID movieId;
 
