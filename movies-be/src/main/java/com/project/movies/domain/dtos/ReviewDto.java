47ae5adb13d65bb7763e2117ff04f1d228b4f970
Harvlin Maximillian <harvlinmaximillian@gmail.com>
2024-12-13 01:56:04 +0700

    Update ReviewDto.java

--- movies-be/src/main/java/com/project/movies/domain/dtos/ReviewDto.java
+++ movies-be/src/main/java/com/project/movies/domain/dtos/ReviewDto.java
@@ -2,10 +2,15 @@ package com.project.movies.domain.dtos;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
-
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
 import java.util.UUID;
 
 @Data
+@AllArgsContructor
+@NoArgsConstructor
+@Builder
 public class ReviewDto {
     UUID reviewId;
 
